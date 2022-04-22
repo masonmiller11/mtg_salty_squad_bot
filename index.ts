25c7114eecafcdf8903deba4c0a251f6ca6d6dbb
Mason Miller <masonmiller11@gmail.com>
2022-04-21 22:45:10 -0400

    Add message-create

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-import { Message } from "discord.js";
-
 const { Client, Intents } = require('discord.js');
 const { config } = require('dotenv');
 const fs = require('node:fs');
@@ -19,18 +17,5 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args: any) => event.execute(...args));
 	}
 }
-
-client.on('message', (message: Message) => {
-	console.log('message!');
-	if (message.content == `!collect`) {
-		// Create a message collector
-		const filter = (m: any) => (m.content.includes('discord'));
-		const channel = message.channel;
-		const collector = channel.createMessageCollector({filter, time: 10000 });
-		console.log("collector started");
-		collector.on('collect', m => console.log(`Collected ${m.content}`));
-		collector.on('end', collected => console.log(`Collected ${collected.size} items`));
-	}
-});
 
 client.login(token);
