7fdb40916d26d2e3499e07eb79ca2557599ce96e
Mason Miller <masonmiller11@gmail.com>
2022-05-05 20:29:15 -0400

    Add winner property to schema

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -3,19 +3,27 @@ import { PlayerCommanderCombatant } from './PlayerCommanderCombatant';
 
 export type Game = {
 	playerCommanderCombatants: PlayerCommanderCombatant[],
-	active: boolean
+	active: boolean,
+	winner: PlayerCommanderCombatant
 }
 
 const GameSchema = new Schema<Game>({
 	playerCommanderCombatants: [
 		{
-			player: {type: String, required: true},
+			player: { type: String, required: true },
 			commander: {
 				type: mongoose.Schema.Types.ObjectId,
 				ref: 'Commander'
 			}
 		}
 	],
+	winner: {
+		player: { type: String },
+		commander: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Commander'
+		}
+	},
 	active: {
 		type: Boolean,
 		required: true
