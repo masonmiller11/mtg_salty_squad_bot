1600362b98ecb9b4ec7b4069380550f843d9dfd4
Mason Miller <masonmiller11@gmail.com>
2022-05-05 20:28:56 -0400

    Add helper methods to game-service

--- src/services/game-service.ts
+++ src/services/game-service.ts
@@ -1,6 +1,7 @@
 import { User } from "discord.js";
 import { GameModel, Game } from "../models/Game";
 import { Document, Types } from 'mongoose';
+import { Commander } from '../models/Commander';
 
 type createGame = (players: User[]) =>
 	Document<unknown, Game> & Game & {
@@ -13,11 +14,19 @@ type getAllActive = () =>
 	})[]>;
 
 type getGame = (id: string) => Promise<(Document<unknown, any, Game> & Game & {
-    _id: Types.ObjectId;
-}) | null>
+	_id: Types.ObjectId;
+}) | null>;
 
-//todo These returns should return models instead of Documents so that we can
-//switch out databases in the future if needed. 
+type addCommander = (gameId: string, playerId: string, commander: Commander) => void;
+
+type setWinner = (gameId: string, playerId: string) => void;
+
+type setInactive = (gameId: string) => void;
+
+type getGamesFromPlayerId = (playerId: string) => Promise<(Document<unknown, any, Game> & Game & {
+	_id: Types.ObjectId;
+})[]>;
+
 
 /**
  * Creates game from players.
@@ -44,11 +53,71 @@ export const createGame: createGame = (players) => {
 }
 
 export const getGame: getGame = async (id: string) => {
-	const game = await GameModel.findById(id);
-	return game;
+	return await GameModel.findById(id).populate('playerCommanderCombatants.commander');
 }
 
 export const getAllActive: getAllActive = async () => {
 	const games = await GameModel.find({ active: true });
 	return games;
+}
+
+export const getGamesWon: getGamesFromPlayerId = async (playerId) => {
+
+	console.log('playerId' + playerId);
+
+	const games = await GameModel.find({ 'winner.player': playerId });
+
+	return games;
+
+}
+
+export const getGamesPlayed: getGamesFromPlayerId = async (playerId) => {
+
+	console.log('playerId' + playerId);
+
+	const games = await GameModel.find({ 'playerCommanderCombatants.player': playerId });
+
+	return games;
+
+}
+
+export const addCommander: addCommander = async (gameId, playerId, commander: Commander) => {
+	const game = await getGame(gameId);
+
+	if (game) {
+
+		game.playerCommanderCombatants.forEach(combatant => {
+			if (combatant.player != playerId) return;
+			combatant.commander = commander;
+		});
+
+		game.save();
+	}
+
+}
+
+export const setWinner: setWinner = async (gameId, playerId) => {
+	const game = await getGame(gameId);
+
+	if (game) {
+
+		const winner = game.playerCommanderCombatants.find(combatant => combatant.player === playerId);
+		console.log('winner' + winner);
+
+		if (winner) game.winner = winner;
+
+		game.save();
+
+	}
+
+}
+
+export const setInactive: setInactive = async (gameId) => {
+	const game = await getGame(gameId);
+
+	if (game) {
+		game.active = false;
+		game.save();
+	}
+
 }
