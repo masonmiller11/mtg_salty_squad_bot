5325890df49fa0600b518a85ceb44ad9e173185d
Mason Miller <masonmiller11@gmail.com>
2022-05-16 00:04:28 -0400

    Clean up

--- src/commands/stats/commander-stats.ts
+++ src/commands/stats/commander-stats.ts
@@ -6,27 +6,24 @@ import * as StatsService from '../../services/stats-service';
 import * as CommanderService from '../../services/model-services/commander-service';
 import * as ErrorService from '../../services/error-service';
 
-
 export const commanderStats: SubCommand = async (interaction: CommandInteraction) => {
 
 	const input = interaction.options.getString('commander');
 
 	if (!input) {
-		ErrorService.interactionReplyError('An error has occurred. Unable to parse subcommand', interaction);
+		ErrorService.interactionReplyError('An error has occurred. Unable to parse subcommand.', interaction);
 		return;
 	}
 
 	const commander = await CommanderService.getCommanderByName(input);
 
 	if (!commander) {
-		console.log('commander is false');
-		await interaction.reply('Commander not found: ' + input);
+		interaction.reply('Commander not found: ' + input);
 		return;
 	}
 
 	const gamesWon = await GameService.getGamesWonByCommander(commander);
 	const gamesPlayed = await GameService.getGamesPlayedByCommander(commander);
-
 	const percentageWon = StatsService.getPercentage(gamesWon.length, gamesPlayed.length);
 
 	const response = `${commander.name} has won ${gamesWon.length}/${gamesPlayed.length} game(s). Winrate: ${percentageWon}%.`;
--- src/commands/stats/player-stats.ts
+++ src/commands/stats/player-stats.ts
@@ -4,14 +4,14 @@ import { SubCommand } from '../../models/Command';
 import * as GameService from '../../services/model-services/game-service';
 import * as StatsService from '../../services/stats-service';
 import * as CommanderStatsService from '../../services/commander-stats-service';
-
+import * as ErrorService from '../../services/error-service';
 
 export const playerStats: SubCommand = async (interaction: CommandInteraction) => {
 
 	const player: User | null = interaction.options.getUser('player');
 
 	if (!player) {
-		interaction.reply('Player could not be found');
+		ErrorService.interactionReplyError('Player could not be found.', interaction);
 		return;
 	}
 
@@ -27,27 +27,33 @@ export const playerStats: SubCommand = async (interaction: CommandInteraction) =
 
 	let response = `${player.username} has won ${gamesWon.length}/${gamesPlayed.length} game(s). Winrate: ${percentageWon}%.`;
 
-	const commanderStatsSortedByPlayedGames = CommanderStatsService.sortCommanderStatsByPlayedGames(gamesPlayed, player.id);
+	const commandersSortedByGames = CommanderStatsService.sortCommanderStatsByPlayedGames(gamesPlayed, player.id);
 	const commandersSortedByWins = CommanderStatsService.sortCommanderStatsByWonGames(gamesWon);
-	const commandersSortedByPercentage = CommanderStatsService.sortCommanderStatsByPercentageWon(commandersSortedByWins, commanderStatsSortedByPlayedGames);
+	const commandersSortedByPerc = CommanderStatsService.sortCommanderStatsByPercentageWon(commandersSortedByWins, commandersSortedByGames);
 
-	response = response + `\n\nMost Played Commanders:\n`;
-	commanderStatsSortedByPlayedGames.slice(0, 5).forEach((commanderHistory, i) =>
-		response = response + `#${i + 1} - ${commanderHistory.commander.name}: ${commanderHistory.gamesPlayed} game(s)\n`
-	);
+	//If we didn't get any commanders back, then do not add commander information to message.
+	//This will happen when a player finishes a game without having a commander set.
+	if (commandersSortedByGames.length > 0) {
 
-	if (commandersSortedByWins.length > 0) {
-
-		response = response + `\nCommanders With The Most Wins:\n`;
-		commandersSortedByWins.slice(0, 5).forEach((commanderHistory, i) =>
-			response = response + `#${i + 1} - ${commanderHistory.commander.name}: ${commanderHistory.gamesWon} win(s)\n`
+		response = response + `\n\nMost Played Commanders:\n`;
+		commandersSortedByGames.slice(0, 5).forEach((commanderHistory, i) =>
+			response = response + `#${i + 1} - ${commanderHistory.commander.name}: ${commanderHistory.gamesPlayed} game(s)\n`
 		);
 
-	}
+		if (commandersSortedByWins.length > 0) {
 
-	response = response + `\nCommanders By Win Percentage:\n`;
-	commandersSortedByPercentage.slice(0, 5).forEach((commanderHistory, i) =>
-		response = response + `#${i + 1} - ${commanderHistory.commander.name}: ${commanderHistory.winPercentage}%\n`);
+			response = response + `\nCommanders With The Most Wins:\n`;
+			commandersSortedByWins.slice(0, 5).forEach((commanderHistory, i) =>
+				response = response + `#${i + 1} - ${commanderHistory.commander.name}: ${commanderHistory.gamesWon} win(s)\n`
+			);
+
+		}
+
+		response = response + `\nCommanders By Win Percentage:\n`;
+		commandersSortedByPerc.slice(0, 5).forEach((commanderHistory, i) =>
+			response = response + `#${i + 1} - ${commanderHistory.commander.name}: ${commanderHistory.winPercentage}%\n`);
+
+	}
 
 	interaction.reply(response);
 
